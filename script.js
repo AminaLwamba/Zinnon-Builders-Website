8784bc734eb9d7da93acd1c84659218e547c92a3
AminaLwamba <aminalwamba81@gmail.com>
2024-11-27 16:42:46 -0800

    modified sliders

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 
-/*Project Details */
-/*project details page*/
-let currentIndex = 0;
-
-function showSlide(index) {
-    const projectSlider = document.querySelector('.project-slider');
-    const projectSlides = document.querySelectorAll('.project-slide');
-    if (index >= projectSlides.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = projectSlides.length - 1;
-    } else {
-        currentIndex = index;
-    }
-    projectSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-document.getElementById('project-next').addEventListener('click', () => {
-    showSlide(currentIndex + 1);
-});
-
-document.getElementById('project-prev').addEventListener('click', () => {
-    showSlide(currentIndex - 1);
-});
 //image counter//
 $(document).ready(function(){
     $('.counter-number').each(function() {
@@ -46,27 +22,12 @@ $(document).ready(function(){
         });
     });
 });
+/*Project Details */
 
 
-//gallery filter
-// gallery filter starts **gallery section (homepage)**
-$(document).ready(function(){
-    $(".list").click(function(){
-        const value=$(this).attr("data-filter");
-        if(value == "all"){
-            $(".gal-photo").show("1000")
-        }
-        else{
-            $(".gal-photo").not("." + value).hide("1000")
-            $(".gal-photo").filter("." + value).show("1000")
-        }
-    })
-    //Active class on buttons
-    $(".list").click(function(){
-        $(this).addClass("active").siblings().removeClass("active")
-    })
-   }) 
-// gallery filter ends **gallery section (homepage)**
+
+
+
 //homepage testimonial slider
 let slideIndex = 1;
 showSlides(slideIndex);
